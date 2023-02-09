981ce07ebda1fe261271cc8b7495b12befadd6d2
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-02-09 15:55:17 +1300

    remove old

--- src/backoffice/settings/relation-types/manifests.ts
+++
@@ -1,5 +0,0 @@
-import { manifests as sidebarMenuItemManifests } from './sidebar-menu-item/manifests';
-import { manifests as treeManifests } from './tree/manifests';
-import { manifests as workspaceManifests } from './workspace/manifests';
-
-export const manifests = [...sidebarMenuItemManifests, ...treeManifests, ...workspaceManifests];
