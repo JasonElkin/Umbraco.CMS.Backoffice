f5efc16fc6b7a9490fc55bf109806371ab45304e
Mads Rasmussen <madsr@hey.com>
2023-03-06 11:21:50 +0100

    rename media tree item

--- src/backoffice/media/media/tree/item/media-tree-item.element.ts
+++ src/backoffice/media/media/tree/item/media-tree-item.element.ts
@@ -2,17 +2,17 @@ import { html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { UmbLitElement } from '@umbraco-cms/element';
 
-@customElement('umb-media-tree-item-label')
-export class UmbMediaTreeItemLabelElement extends UmbLitElement {
+@customElement('umb-media-tree-item')
+export class UmbMediaTreeItemElement extends UmbLitElement {
 	render() {
 		return html`<div>MEDIA LABEL FROM EXTENSION</div>`;
 	}
 }
 
-export default UmbMediaTreeItemLabelElement;
+export default UmbMediaTreeItemElement;
 
 declare global {
 	interface HTMLElementTagNameMap {
-		'umb-media-tree-item-label': UmbMediaTreeItemLabelElement;
+		'umb-media-tree-item': UmbMediaTreeItemElement;
 	}
 }
