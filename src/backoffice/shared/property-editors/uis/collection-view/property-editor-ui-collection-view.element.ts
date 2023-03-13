11c55036074d7e5108b97c0c532fd3b097852604
Niels Lyngsø <nsl@umbraco.dk>
2023-03-13 14:16:16 +0100

    UmbPropertyEditorUICollectionViewElement interface

--- src/backoffice/shared/property-editors/uis/collection-view/property-editor-ui-collection-view.element.ts
+++ src/backoffice/shared/property-editors/uis/collection-view/property-editor-ui-collection-view.element.ts
@@ -1,13 +1,14 @@
 import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { UUITextStyles } from '@umbraco-ui/uui-css/lib';
+import { UmbPropertyEditorElement } from '@umbraco-cms/property-editor';
 import { UmbLitElement } from '@umbraco-cms/element';
 
 /**
  * @element umb-property-editor-ui-collection-view
  */
 @customElement('umb-property-editor-ui-collection-view')
-export class UmbPropertyEditorUICollectionViewElement extends UmbLitElement {
+export class UmbPropertyEditorUICollectionViewElement extends UmbLitElement implements UmbPropertyEditorElement {
 	static styles = [UUITextStyles];
 
 	@property()
